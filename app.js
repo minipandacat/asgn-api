4755bbc023a16d8dfb4aa400df08701afda1d464
Joan Kuker <minipandacat@users.noreply.github.com>
2021-05-06 16:17:13 -0500

    commit

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-console.log(process.env);
-const mongoose = require(mongoose);
-mongoose.connect(process.env.MONGO_URI, {useNewURLParser: true});
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.json());
 
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
+var personRoute = require('./asgn-router/')
 
 app.get('/', (req, res) => {
     res.send('Successful response.');
   });
+
+  console.log(process.env.MONGO_URI);
+  const username = process.env.username;
+  const password = process.env.password;
+  const server = process.env.server;
+  const database = process.env.database;
+
+  const mongoose = require(mongoose);
+  mongoose.connect(process.env.MONGO_URI, {useNewURLParser: true});
 
 const server = ''; // REPLACE WITH YOUR DB SERVER
 const database = '';      // REPLACE WITH YOUR DB NAME
